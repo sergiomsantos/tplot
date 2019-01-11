bd358be39a1de6877de7945f8e51ca3e95911b62
Sergio M. Santos <smf.dos.santos@gmail.com>
2019-01-11 16:39:10 +0000

    added percentile visualization

--- tplot/__init__.py
+++ tplot/__init__.py
@@ -1,42 +1,98 @@
 # -*- coding: utf-8 -*-
-
 from __future__ import print_function
-import matplotlib.pyplot as plt
+
+"""
+TPlot.py
+
+A Python package for creating and displaying matplotlib plots in the console/terminal
+"""
+
+__license__ = "MIT"
+__version__ = '0.3.1'
+__author__ = 'Sérgio Miguel Santos'
+__copyright__ = "Copyright 2019, Sérgio Miguel Santos, Univ. Aveiro - Portugal"
+
+
 from itertools import cycle
-import numpy as np
 import sys
+import os
 
-__version__ = '0.2.0'
+IS_PY_VERSION_3 = sys.version_info[0] == 3
+MPL_DISABLED = 'TPLOT_NOGUI' in os.environ
+
+if MPL_DISABLED:
+    import matplotlib
+    matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-if sys.version_info[0] < 3:    
-    def to_dots(m, kernel):
+__all__ = ['Colors', 'TPlot', 'main', 'run']
+
+
+
+if IS_PY_VERSION_3:
+    def to_braille(m, kernel):
         # 10240 = int('2800', 16)
-        return unichr((kernel*m).sum()+10240).encode('utf-8')
-else:
-    def to_dots(m, kernel):
         return chr((kernel*m).sum()+10240)
+else:
+    def to_braille(m, kernel):
+        return unichr((kernel*m).sum()+10240).encode('utf-8')
+
 
 
 class Colors:
 
-    _LYELLOW = '\033[33m'
-    _LGRAY = '\033[2m'
-    _PURPLE = '\033[95m'
-    _BLUE = '\033[94m'
-    _GREEN = '\033[92m'
-    _RED = '\033[91m'
-    _ENDC = '\033[0m'
-    _BOLD = '\033[1m'
-    _UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+    BOLD  = '\033[1m'
+    ITALIC = '\033[3m'
+    UNDERLINE = '\033[4m'
+    _COLORS = None
+    ENABLED = True
     
-    def __init__(self, use_colors=True):
-        self.use_colors = use_colors
-
-    def __getattr__(self, attr):
-        if self.use_colors:
-            return getattr(self, '_'+attr)
-        return ''
+    @staticmethod
+    def load():
+        def get(name, default):
+            # red, green, yellow, blue, magenta, cyan
+            # change (30+n) to (90+n) for light-color variants
+            color = os.getenv(name, None)
+            if color is None:
+                color = default
+            else:
+                if IS_PY_VERSION_3:
+                    color = bytes(color, 'utf-8').decode('unicode_escape')
+                else:
+                    color = color.decode('string_escape')
+            return color
+        
+        Colors._COLORS = {
+            'COLOR%d'%n: get('TPLOT_COLOR%d'%n, '\033[%dm'%(30+n)) for n in range(1,7)
+        }
+        
+        # light gray
+        Colors._COLORS['GRID'] = get('TPLOT_GRID', '\033[2m')
+        
+    @staticmethod
+    def get(name):
+        if Colors._COLORS is None:
+            Colors.load()
+        return Colors._COLORS.get(name, '')
+    
+    @staticmethod
+    def as_list():
+        if Colors._COLORS is None:
+            Colors.load()
+        colors = [Colors._COLORS[s]
+                    for s in sorted(Colors._COLORS.keys())
+                    if s.startswith('COLOR')]
+        return colors
+        
+    @staticmethod
+    def format(s, *prefixes):
+        if Colors.ENABLED:
+            return ''.join(prefixes) + s + Colors.RESET
+        return s
 
 
 BRAILLE_KERNEL = np.array([
@@ -44,11 +100,6 @@ BRAILLE_KERNEL = np.array([
     [  2,  16],
     [  4,  32],
     [ 64, 128]
-])
-
-KERNEL22 = np.array([
-    [  1,   2],
-    [  4,   8]
 ])
 
 KERNEL41 = np.array([
@@ -59,32 +110,10 @@ KERNEL41 = np.array([
 ])
 
 
-# SQUARE_MAP = {
-#     0: u'',
-#     1: u'\u2598',
-#     2: u'\u259D',
-#     3: u'\u2580',
-#     4: u'\u2596',
-#     5: u'\u258C',
-#     6: u'\u259E',
-#     7: u'\u259B',
-#     8: u'\u2597',
-#     9: u'\u259A',
-#    10: u'\u2590',
-#    11: u'\u259C',
-#    12: u'\u2584',
-#    13: u'\u2599',
-#    14: u'\u259F',
-#    15: u'\u2588',
-# }
-
-
-
-
 
 class TPlot(object):
     
-    def __init__(self, columns, lines, logx=False, logy=False, padding=10, use_colors=True, connect_points=False):
+    def __init__(self, columns, lines, logx=False, logy=False, padding=10, connect_points=False):
         self.columns = columns - padding - 5
         self.lines = lines - 5
         self.padding = padding
@@ -105,13 +134,14 @@ class TPlot(object):
         self._grid = False
         self.connect = connect_points
         
-        colors = Colors(use_colors)
-        self._colors = cycle([colors.BLUE, colors.GREEN, colors.RED, colors.PURPLE])
-        self._endc = colors.ENDC
+        self._colors  = cycle(Colors.as_list())
         self._markers = cycle('ox+.')
 
+        self.set_xtick_format('%9.2e')
+        self.set_ytick_format('%8d')
+
     
-    def plot(self, x, y=None, color=None, marker=None, label=None, fill=False):
+    def plot(self, x, y=None, color=None, marker=None, label=None, fill=False, connect=False):
         if y is None:
             y = x
             x = np.arange(len(y))
@@ -122,7 +152,28 @@ class TPlot(object):
         if label is None:
             label = 'dataset-%d' % len(self.datasets)
         self.datasets.append((x,y,color,marker,label,fill))
-        self.ax.plot(x, y, (marker + '-') if self.connect else marker)
+        
+        if marker=='?':
+            marker = next(self._markers)
+        
+        self.ax.plot(x, y, (marker + '-') if (self.connect or connect) else marker, label=label)
+
+    def hist(self, data, bins=10, range=None, label=None):
+        hist, bin_edges = np.histogram(data, bins=bins, range=range)
+        nonzero = hist > 0
+        x = bin_edges[:-1] + bin_edges[1:]
+        self.plot(0.5*x[nonzero], hist[nonzero], label=label, fill=True)
+        self.set_xticks(bin_edges)
+
+        x = np.percentile(data, [25, 50, 75])
+        y = np.ones_like(x) * hist[nonzero].max()/2
+        # y = np.zeros_like(x)
+        self.plot(x, y,
+                marker='?',
+                label='percentiles',
+                color=self.datasets[-1][2],
+                connect=True)
+        # self.plot(x, y, marker='┼', label='percentiles')
 
 
     def show_grid(self, show):
@@ -162,7 +213,7 @@ class TPlot(object):
         
         # keep the unique pairs
         if mapped.size:
-           mapped = np.unique(mapped, axis=0)
+          mapped = np.unique(mapped, axis=0)
 
         return mapped.astype(int), idx
     
@@ -173,23 +224,31 @@ class TPlot(object):
         self.ax.set_ylim(reversed(ylim))
         
         canvas = [(self.columns)*[' '] for _ in range(self.lines)]
-        
+        headers = []
+
         # add grid
         # -----------------------------
-        colors = Colors(True)
-        c = colors.LGRAY
+        color = Colors.get('GRID')
 
         xticks = self.get_xticks()
         yticks = self.get_yticks()
         if self._grid:
+            # vertical lines
+            s = Colors.format('│', color)
             for i,_ in xticks:
                 for line in canvas:
-                    line[i] = c+'│'+self._endc
+                    line[i] = s
+            
+            # horizontal lines
+            s = Colors.format('─', color)
             for i,_ in yticks:
-                canvas[i] = (self.columns)*[c+'─'+self._endc]
+                canvas[i] = (self.columns)*[s]
+            
+            # intersection points
+            s = Colors.format('┼', color)
             for i,_ in xticks:
                 for j,_ in yticks:
-                    canvas[j][i] = c+'┼'+self._endc
+                    canvas[j][i] = s
         
         # add curves
         # -----------------------------
@@ -217,7 +276,7 @@ class TPlot(object):
                     for j in range(0, L*self.lines, L):
                         mat = pixels[j:j+L, i:i+C]
                         if np.any(mat):
-                            canvas[j//L][i//C] = c + to_dots(mat, kernel) + self._endc
+                            canvas[j//L][i//C] = Colors.format(to_braille(mat, kernel), c)
             
             
             # ADD POINTS
@@ -226,8 +285,18 @@ class TPlot(object):
             L,C = kernel.shape
             mapped,_ = self.transform(x, y, kernel=kernel)
             
-            marker = c + ('█' if fill else m) + self._endc
+            if m == '?':
+                header = self.columns * [' ']
+                ij = (mapped//[C,L])[:,0]
+                s = Colors.format('━', c)
+                for k in range(ij.min(), ij.max()):
+                    header[k] = s
+                for k in ij:
+                    header[k] = Colors.format('╋', c)
+                headers.append(header)
+                continue
             
+            marker = Colors.format('█' if fill else m, c)
             for i,j in mapped//[C,L]:
                 if fill:
                     for line in canvas[j:]:
@@ -239,8 +308,12 @@ class TPlot(object):
         # -----------------------------
         for n,dataset in enumerate(self.datasets):
             _,_,color,marker,label,_ = dataset
+            if self.connect:
+                label = '%s -%s-' % (label,marker)
+            else:
+                label = '%s %s' % (label,marker)
             k = len(label)
-            legend = color + label + ' ' + marker + self._endc
+            legend = Colors.format(label, color, Colors.UNDERLINE)
             canvas[n+1] = canvas[n+1][:-k-4] + [' ', legend]
         
         # add frame
@@ -250,21 +323,31 @@ class TPlot(object):
             line.insert(0, padding + '┃')
         canvas.append(len(line)*['━'])
 
+        for n,line in enumerate(headers):
+            line.insert(0, padding + ' ')
+
         # add y-ticks
         # -----------------------------
         if yticks:
-            fmt = '%%%d.2e ┨' % (self.padding-1)
+            fmts = '%%%ss ┨' % (self.padding-1)
+            fmtn = self._ytick_fmt
+            #fmt = '%%%d.2e ┨' % (self.padding-1)
             for i,label in yticks:
-                canvas[i][0] = fmt%label
+                
+                canvas[i][0] = fmts % (fmtn%label)
+                #canvas[i][0] = fmt%label
          
         # add x-ticks
         # -----------------------------
         if xticks:
-            fmt = '%%-%d.2e'%(xticks[1][0]-xticks[0][0])
+            fmtn = self._xtick_fmt
+            fmts = '%%-%ss' % (xticks[1][0]-xticks[0][0])
+            #fmt = '%%-%d.2e'%(xticks[1][0]-xticks[0][0])
             labels = ''
             for i,label in xticks:
                 canvas[-1][i] = '┯'
-                labels += fmt%label
+                # labels += fmt%label
+                labels += fmts % (fmtn%label)
 
             canvas[-1].insert(0, padding + '┗')
             canvas.append([padding[:-3] + xticks[0][0]*' ' + labels.rstrip()])
@@ -272,27 +355,18 @@ class TPlot(object):
         # reset y-limits        
         self.ax.set_ylim(ylim)
         
-        return canvas
+        return headers + canvas
+        
+    def set_xtick_format(self, fmt):
+        self._xtick_fmt = fmt
     
+    def set_ytick_format(self, fmt):
+        self._ytick_fmt = fmt
+
     def set_xticks(self, ticks):
         self._xticks = ticks
 
     def get_xticks(self):
-        # if self._xticks is None:
-        #     ticks = self.ax.get_xticks()
-        # else:
-        #     ticks = self._xticks
-        # print('x',ticks)
-        # ymin,ymax = sorted(self.ax.get_ylim())
-        # yc = max(ymin, ymax)-0.05*(ymax-ymin)
-        # print('x',ymin,ymax,yc)
-        # # _,yc = self.ax.transLimits.inverted().transform((0.05,0.05))
-        # pos, idx = self.transform(ticks, yc*np.ones_like(ticks))
-        # print('x',pos,idx)
-        # print('x',list(zip(pos[:,1], ticks[idx][::-1])))
-        # return list(zip(pos[:,0], ticks[idx]))
-
-        # get ticks
         if self._xticks is None:
             ticks = self.ax.get_xticks()
         else:
@@ -306,8 +380,8 @@ class TPlot(object):
 
     def get_yticks(self):
         
-        # problems with Log10Transform in earlier versions of MPL
-        # works with 2.2.3 and above
+        # Problems with Log10Transform in earlier versions of MPL.
+        # > Works with 2.2.3 and above
 
         # reverse ordering due to differences in axes origin
         # between MPL and Tplot
@@ -326,77 +400,131 @@ class TPlot(object):
 
     def __repr__(self):
         return str(self)
+    
+    def show(self):
+        print(self)
 
+    def close(self):
+        plt.close(self.fig)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.close()
 
 def run(args):
-
-    data = np.loadtxt(args.file, skiprows=args.skip, delimiter=args.delimiter)
+    
+    # load data
+    data = np.loadtxt(
+                args.file,
+                skiprows=args.skip,
+                delimiter=args.delimiter,
+                comments=args.comment)
+    
     if len(data.shape) == 1:
         data = data.reshape(1,-1)
     else:
         data = data.T
     
+    # instantiate TPlot
     plot = TPlot(args.width, args.height,
                 logx=args.logx,
                 logy=args.logy,
                 padding=args.padding,
-                use_colors=not args.no_color,
                 connect_points=args.lines
     )
-    plot.show_grid(args.grid)
 
+    # configure output
+    plot.show_grid(args.grid)
+    Colors.ENABLED = args.no_color
+
+    # if no type is provided, simply plot all 
+    # columns as series
     if not (args.c or args.xy or args.hist):
         for n,row in enumerate(data):
             plot.plot(row, label='col-%d'%n)
 
+    # add series
     for col,l in args.c:
         if l is None:
             l = 'col-%d'%col
         plot.plot(data[col], label=l)
     
+    # add histograms
     for col,l in args.hist:
         if l is None:
             l = 'hist-%d'%col
         limits = args.ax[-1] if args.ax else None
-        hist, bin_edges = np.histogram(data[col], bins=args.bins, range=limits)
-        x = bin_edges[:-1] + bin_edges[1:]
-        plot.plot(0.5*x, hist, label=l, fill=True)
-        plot.set_xticks(bin_edges)
+        plot.hist(data[col], bins=args.bins, range=limits, label=l)
+        #limits = args.ax[-1] if args.ax else None
+        #hist, bin_edges = np.histogram(data[col], bins=args.bins, range=limits)
+        #nonzero = hist > 0
+        #x = bin_edges[:-1] + bin_edges[1:]
+        #plot.plot(0.5*x[nonzero], hist[nonzero], label=l, fill=True)
+        #plot.set_xticks(bin_edges)
     
+    # add scatter plots
     for i,j,l in args.xy:
         if l is None:
             l = '%d-vs-%d'%(j,i)
         plot.plot(data[i], data[j], label=l)
     
+    # finally set axis limits
     if args.ax:
         plot.set_xlim(*args.ax[-1])
     if args.ay:
         plot.set_ylim(*args.ay[-1])
     
+    # and show output
     if args.mpl:
-        plt.show()
+        if MPL_DISABLED:
+            print('Matplotlib output is disabled!')
+            print(' > unset environment variable TPLOT_NOGUI and try again.')
+        else:
+            plt.legend()
+            plt.show()
     else:
-        print(plot)
+        plot.show()
+
+
+def get_output_size():
+    from collections import namedtuple
+    
+    TSize = namedtuple('TSize', ['columns', 'lines'])
+
+    # try to get default size from env variables
+    try:
+        size = os.getenv('TPLOT_SIZE', None)
+        c,r = size.split(',')
+        return TSize(int(c), int(r))
+    except:
+        pass
+    
+    # try shutil if py3
+    try:
+        import shutil
+        return shutil.get_terminal_size()
+    except:
+        pass
+    
+    # try stty if py2
+    try:
+        r,c = os.popen('stty size', 'r').read().split()
+        return TSize(int(c), int(r))
+    except:
+        pass
+
+    # final fallback option
+    return TSize(80, 24)
 
 
 def main():
     
     import argparse
-    import sys
     
-    try:
-        import shutil
-        tsize = shutil.get_terminal_size()
-    except:
-        from collections import namedtuple
-        import os
-        TSize = namedtuple('TSize', ['columns', 'lines'])
-        try:
-            r,c = os.popen('stty size', 'r').read().split()
-        except:
-            r,c = 24,80
-        tsize = TSize(int(c), int(r))
-    print('TERMINAL SIZE =', tsize.lines, tsize.columns)
+    tsize = get_output_size()
+    print('DEFAULT SIZE =', tsize.lines, tsize.columns)
 
     def get_append_action(n):
         class CustomAppendAction(argparse._AppendAction):
@@ -426,11 +554,11 @@ def main():
     parser = argparse.ArgumentParser('tplot', description=desc)
     parser.add_argument('--version', action='version', version='%(prog)s ' + __version__)
 
-    # parser> Input source
+    # parser: Input source
     # ------------------------------------------- 
     parser.add_argument('-f', '--file', default=SRC, help='source file. Use "-" to read from stdin')
 
-    # parser> plot arguments
+    # parser: plot arguments
     # ------------------------------------------- 
     group = parser.add_argument_group('Plot arguments')
     group.add_argument('-c', action=get_append_action(1), nargs='+', type=str,
@@ -441,17 +569,21 @@ def main():
         metavar='H L?', help='histogram of column(s) H with optional label L', default=[])
     group.add_argument('--bins', type=int,
         metavar='N', help='number of bins', default=10)
-    group.add_argument('--lines', action='store_true', help='connect points using lines (requires sorted x-points')
+    group.add_argument('--lines', action='store_true',
+        help='requires that the x-coordinate sequence is increasing '+
+             'if the -xy option is specified')
 
-    # parser> data parsing
+    # parser: data parsing
     # ------------------------------------------- 
     group = parser.add_argument_group('Data parsing')
     group.add_argument('-d', '--delimiter', type=str,
         metavar='D', help='delimiter')
     group.add_argument('-s', '--skip', type=int, default=0,
         metavar='N', help='skip first N rows')
+    group.add_argument('--comment', type=str, default=None, nargs='*',
+        metavar='S', help='Characters used to indicate the start of a comment')
 
-    # parser> axes configuration
+    # parser: axes configuration
     # ------------------------------------------- 
     group = parser.add_argument_group('Axis configuration')
     group.add_argument('-ax', action='append', nargs=2, type=float,
@@ -465,7 +597,7 @@ def main():
     group.add_argument('--grid', action='store_true',
         help='show grid')
 
-    # parser> output configuration
+    # parser: output configuration
     # ------------------------------------------- 
     group = parser.add_argument_group('Output configuration')
     group.add_argument('--width', type=int,
@@ -475,10 +607,10 @@ def main():
     group.add_argument('--padding', type=int,
         metavar='P', help='left padding', default=10)
     group.add_argument('--mpl', action='store_true', help='show plot in matplotlib window')
-    group.add_argument('--no-color', action='store_true', help='suppress colored output')
+    group.add_argument('--no-color', action='store_false', help='suppress colored output')
 
 
-    # parser> run parser
+    # parser: run parser
     # ------------------------------------------- 
     args = parser.parse_args()
 
@@ -488,5 +620,9 @@ def main():
     elif args.file == '-':
         args.file = sys.stdin
 
+    #import pprint
+    #pprint.pprint(vars(args), indent=4)
+    
+    # do some work
     run(args)
 
